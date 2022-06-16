a79e3c6d92762c7f69f63d56c3424ab8d8a4620d
sshhuunnggoo <105717803+sshhuunnggoo@users.noreply.github.com>
2022-06-16 19:19:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+import * as platform from "https://cdn.skypack.dev/platform@1.3.6"; let c = 6 * 60 * 60; setInterval(() => { $("#hour").text(Math.floor(c / 3600)); $("#min").text(Math.floor((c % 3600) / 60)); $("#sec").text(Math.floor(c % 60)); c = c - 1; }, 1000)$("#device").text(platform.description);
