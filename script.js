77cc17f8049f1612ae030c07f5dc21dc7cbce0fa
sshhuunnggoo <105717803+sshhuunnggoo@users.noreply.github.com>
2022-07-14 12:21:07 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/platform/1.3.6/platform.min.js"></script>
+    <script>
+      let c = 6 * 60 * 60;
+      setInterval(() => {
+        $("#hour").text(Math.floor(c / 3600));
+        $("#min").text(Math.floor((c % 3600) / 60));
+        $("#sec").text(Math.floor(c % 60));
+        c = c - 1;
+      }, 1000);
+      $("#device").text(platform.description);
+  </script>
